7c238c21d96aae8e812f03dd834b828968bee125
svvdevell <svvdevel@gmail.com>
2022-09-05 11:09:56 +0300

    added 1st task

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
+"use strict"
+
+// 1 
+
+// function sumArr() {
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log(arguments[i]);
+//     }
+//     if (arguments.length <= 2){
+//         console.error("Error...");
+//     }
+//     else{
+//         let arr = Array.from(arguments); 
+//         // console.log(arr);
+//         let res = arr.sort((a,b)=> a - b).slice(0,2);
+//         console.log(res);
+//         let sum = res[0] + res[1];
+//         console.log(sum);
+//     }
+// }
+
+// sumArr(1,223,53444,77,34,67,9);
 
